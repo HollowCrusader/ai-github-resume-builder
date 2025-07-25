df86d81155c4a8cc90546b23a126e2589c436128
Aaron Parker <ajpccl@rit.edu>
2025-07-25 11:37:07 -0400

    add skeleton for main.py and commands

---
+++ commands/render.py
@@ -0,0 +1,7 @@
+import typer
+
+def generate_resume():
+    """Generate a resume from the resume.yaml file."""
+    
+    typer.echo("Generating resume from resume.yaml...")
+    # Logic to read resume.yaml and generate the resume would go here
---
+++ commands/skills.py
@@ -0,0 +1,7 @@
+import typer
+
+def add_skills():
+    """Add skills to the resume.yaml file."""
+    
+    typer.echo("Adding skills to resume.yaml...")
+    # Logic to add skills to resume.yaml would go here
